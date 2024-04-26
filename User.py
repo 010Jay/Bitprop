7c8463a86e4e19e6308fb0467c416a08889ef085
Jay010 <jsnjaftha3@gmail.com>
2024-04-26 21:37:18 +0200

    Create the User class and the user database impl

---
+++ User.py
@@ -0,0 +1,9 @@
+class User:
+
+    def __init__(self, user_id, first_name,  last_name, email_address, cellphone):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email_address = email_address
+        self.cellphone = cellphone
+
---
+++ UserDatabase.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect('user.db')
+cur = conn.cursor()
+
+# cur.execute("""
+#                 CREATE TABLE user (
+#                 user_id integer PRIMARY KEY,
+#                 first_name text,
+#                 last_name text,
+#                 email_address text,
+#                 cellphone text
+#             )""")
+
+# cur.execute(" INSERT INTO user VALUES (null, 'Stephan', 'Salvatore', 'salvatores@gamil.com', '0653815478')")
+
+cur.execute("SELECT * FROM user")
+print(cur.fetchall())
+
+conn.commit()
+conn.close()
+
